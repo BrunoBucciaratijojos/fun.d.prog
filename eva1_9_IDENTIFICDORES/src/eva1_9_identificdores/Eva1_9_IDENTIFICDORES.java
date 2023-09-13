f5a980e47e1eb02cd552362bec3e6125bf8b4fb4
BrunoBucciaratijojos <143563080+BrunoBucciaratijojos@users.noreply.github.com>
2023-09-13 17:53:44 -0600

    Add files via upload

---
+++ eva1_9_IDENTIFICDORES/src/eva1_9_identificdores/Eva1_9_IDENTIFICDORES.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eva1_9_identificdores;
+
+/**
+ *
+ * @author invitado
+ */
+public class Eva1_9_IDENTIFICDORES {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //int 1nombre; nombre no valido, empieza con un número
+        int nombre;
+        int $nombre;
+        int nombre1;
+        int _nombre;
+        int Nombre; 
+        //int nombre; la variable ya existe preivamente   
+        
+        //int public; no podemos usar public, es palabra reservada (keyword)
+        int Public;
+    }
+    
+}
