3cf907ce8de50d30a4de8615a9cee41f33f8cce1
BrunoBucciaratijojos <143563080+BrunoBucciaratijojos@users.noreply.github.com>
2023-09-18 17:52:02 -0600

    Add files via upload

---
+++ eva1_10_INICIALIZACION/src/eva1_10_inicializacion/Eva1_10_INICIALIZACION.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eva1_10_inicializacion;
+
+/**
+ *
+ * @author Invitado
+ */
+public class Eva1_10_INICIALIZACION {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int valor = 0;//inicializando la variable
+        System.out.println("valor");
+        int precio; //declarar
+        precio = 100;//inicializado
+        
+        double salario = 0, precioMAYOREO = 100, iva =10.5, subtototal =15, total=100; // tenemos cinco valores
+        
+        //constante
+        //final --> te permite asignar el valor una vez
+        final int conteo;
+        conteo=100;
+        System.out.println("conteo");
+        final double MI_VALOR_PI =3.1416;
+        final String ESCUELA = "instituto tecnologico de chihuahua11";
+        final int califa_pasar = 70;
+        
+        /*
+        varias lineas de comentarios cuando quieren tirar mucho rollo esta es la mejor opción
+                
+        */
+        int x; //comentario de unasola linea, todo es una linea de comentario 
+    }
+    
+    
+}
